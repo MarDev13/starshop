6cadb56e0169265bf45870127efa88248e7290a1
Mar <mardev@gmail.com>
2025-02-07 20:55:57 +0100

    add prefijo route

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -8,14 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships/')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships/{id<\d+>}', methods:['GET','POST'])]
-    public function getCollection(LoggerInterface $logger, StarshipRepository $repository, int $id = 0): Response
+    #[Route('', methods:['GET','POST'])]
+    public function getCollection( StarshipRepository $repository): Response
     {
-        dd($id);
+        
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods:['GET','POST'])]
+    public function get(StarshipRepository $repository, int $id){
+        $starships = $repository->findAll();
+        return $this->json($starships[$id]);
     }
 }
