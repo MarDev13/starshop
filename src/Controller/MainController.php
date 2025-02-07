1bdceff173ac9f2e89bdecb1229ea56aeb0c9951
Mar <mardev@gmail.com>
2025-02-07 18:33:48 +0100

    add name route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/','homepage')]
     public function homepage(): Response
     {
         $starshipCount = 457;
